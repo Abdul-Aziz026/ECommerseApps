4b2098473a4cda8f116fca9d16516d90fed740ea
Azizur Rahman <azizur.rahman@orbitax.com>
2024-11-25 10:32:07 +0600

    update till section 12

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
+import { MatProgressBar, MatProgressBarModule } from '@angular/material/progress-bar';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    MatProgressBar
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 
 export class HeaderComponent {
+  busyService = inject(BusyService);
 
 }
--- client/src/app/shared/server-error/server-error.component.ts
+++ client/src/app/shared/server-error/server-error.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './server-error.component.html',
   styleUrl: './server-error.component.css'
 })
