1816a64961b09b51206cc5ab13fea13103609601
Azizur Rahman <azizur.rahman@orbitax.com>
2024-11-28 17:17:25 +0600

    update before identity

--- client/src/app/features/cart/cart-item/cart-item.component.ts
+++ client/src/app/features/cart/cart-item/cart-item.component.ts
@@ -1,13 +1,31 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { CartItem } from '../../../shared/models/cart';
 import { RouterLink } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { CartService } from '../../../core/services/cart.service';
 
 @Component({
   selector: 'app-cart-item',
-  imports: [RouterLink],
+  imports: [RouterLink, MatIcon],
   templateUrl: './cart-item.component.html',
   styleUrl: './cart-item.component.css'
 })
 export class CartItemComponent {
   item = input.required<CartItem>();
+  cartService = inject(CartService);
+
+  incrementQuantity() {
+    this.cartService.addItemToCart(this.item());
+  }
+
+  
+  decrementQuantity() {
+    this.cartService.removeItemFromCart(this.item().productId, 1);
+  }
+  
+  removeItemFromCart() {
+    console.log("Called remove");
+    this.cartService.removeItemFromCart(this.item().productId, this.item().quantity);
+    
+  }
 }
